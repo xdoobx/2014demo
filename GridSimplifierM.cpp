1d64744e7b127cb439fb826df030d4df1142e036
ylu <2295857079@qq.com>
2014-07-08 11:31:26 -0400

    Update format

--- GridSimplifierM.cpp
+++ GridSimplifierM.cpp
@@ -4,7 +4,6 @@
 #include "FileIO.h"
 #include <thread>
 
-
 GridSimplifierM::GridSimplifierM(char* lineFile, char* pointFile){
 	//clock_t begin, end;
 	//begin = clock();
@@ -50,29 +49,84 @@ bool GridSimplifierM::removeS(Polygon &poly, int threadId){
 
 void GridSimplifierM::simplifyT(vector<Line*> lines, Triangle& tri, int threadId){
 	for (int i = 0; i<lines.size(); ++i){
-		for (int j = 1; j < lines[i]->points.size() - 1; ++j){
-			tri.p[1] = lines[i]->points[j];
-			tri.p[0] = lines[tri.p[1]->lineInd]->points[tri.p[1]->leftInd];
-			tri.p[2] = lines[tri.p[1]->lineInd]->points[tri.p[1]->rightInd];
-			tri.sort();
-			removeS(tri,threadId);
+		if (lines[i]->points[0] != lines[i]->points[lines[i]->points.size() - 1]){
+			for (int j = 1; j < lines[i]->points.size() - 1; ++j){
+				tri.p[1] = lines[i]->points[j];
+				tri.p[0] = lines[tri.p[1]->lineInd]->points[tri.p[1]->leftInd];
+				tri.p[2] = lines[tri.p[1]->lineInd]->points[tri.p[1]->rightInd];
+				tri.sort();
+				removeS(tri, threadId);
+			}
+		}
+		else{
+			for (int j = 1; j < lines[i]->points.size() - 3; ++j){
+				tri.p[1] = lines[i]->points[j];
+				tri.p[0] = lines[tri.p[1]->lineInd]->points[tri.p[1]->leftInd];
+				tri.p[2] = lines[tri.p[1]->lineInd]->points[tri.p[1]->rightInd];
+				tri.sort();
+				removeS(tri, threadId);
+			}
 		}
 	}
 }
 
 void GridSimplifierM::simplifyTP(vector<Line*> lines, Polygon& poly, int threadId){
+	Triangle tri;
 	for (int i = 0; i<lines.size(); ++i){
-		for (int j = 1; j < lines[i]->points.size() / 2; ++j){
+		/*for (int j = 1; j < lines[i]->points.size() / 2; ++j){
 			poly.p[1] = lines[i]->points[j * 2 - 1];
 			poly.p[2] = lines[i]->points[j * 2];
 			poly.p[0] = lines[i]->points[poly.p[1]->leftInd];
 			poly.p[3] = lines[i]->points[poly.p[2]->rightInd];
 			poly.getRange();
-			removeS(poly, threadId);
+
+			if (!removeS(poly, threadId)){
+				if(!removeS(Triangle(poly.p[1], poly.p[2], poly.p[3]), threadId))
+					removeS(Triangle(poly.p[0], poly.p[1], poly.p[2]), threadId);
+			}
 		}
 		if (lines[i]->points.size() % 2 == 1){
-			Triangle tri;
 			tri.p[1] = lines[i]->points[lines[i]->points.size()-2];
+			tri.p[0] = lines[i]->points[tri.p[1]->leftInd];
+			tri.p[2] = lines[i]->points[tri.p[1]->rightInd];
+			tri.sort();
+			removeS(tri, threadId);
+		}*/
+		for (int j = 1; j <= (lines[i]->points.size()-2) / 3; ++j){
+			poly.p[1] = lines[i]->points[j * 3 - 2];
+			poly.p[2] = lines[i]->points[j * 3 - 1];
+			poly.p[3] = lines[i]->points[j * 3];
+			poly.p[0] = lines[i]->points[poly.p[1]->leftInd];
+			poly.p[4] = lines[i]->points[poly.p[3]->rightInd];
+			poly.getRange();
+
+			if (!removeS(poly, threadId)){
+				if (!removeS(Triangle(poly.p[2], poly.p[3], poly.p[4]), threadId)){
+					if(!removeS(Triangle(poly.p[1], poly.p[2], poly.p[3]), threadId))
+						removeS(Triangle(poly.p[0], poly.p[1], poly.p[2]), threadId);
+					else
+						removeS(Triangle(poly.p[0], poly.p[1], poly.p[3]), threadId);
+				}
+				else if (!removeS(Triangle(poly.p[1], poly.p[2], poly.p[4]), threadId))
+					removeS(Triangle(poly.p[0], poly.p[1], poly.p[2]), threadId);
+				else
+					removeS(Triangle(poly.p[0], poly.p[1], poly.p[4]), threadId);
+			}
+		}
+		if ((lines[i]->points.size() - 2) % 3 == 1){
+			tri.p[1] = lines[i]->points[lines[i]->points.size() - 2];
+			tri.p[0] = lines[i]->points[tri.p[1]->leftInd];
+			tri.p[2] = lines[i]->points[tri.p[1]->rightInd];
+			tri.sort();
+			removeS(tri, threadId);
+		}
+		else if ((lines[i]->points.size() - 2) % 3 == 2){
+			tri.p[1] = lines[i]->points[lines[i]->points.size() - 3];
+			tri.p[0] = lines[i]->points[tri.p[1]->leftInd];
+			tri.p[2] = lines[i]->points[tri.p[1]->rightInd];
+			tri.sort();
+			removeS(tri, threadId);
+			tri.p[1] = lines[i]->points[lines[i]->points.size() - 2];
 			tri.p[0] = lines[i]->points[tri.p[1]->leftInd];
 			tri.p[2] = lines[i]->points[tri.p[1]->rightInd];
 			tri.sort();
@@ -100,11 +154,14 @@ void GridSimplifierM::simplifyMT(int limit){
 }
 
 void GridSimplifierM::simplifyMTP(int limit){
-	Polygon poly1(4, new Point*[4]);
+	Polygon poly1(5, new Point*[5]);
+	Polygon poly2(5, new Point*[5]);
+	Polygon poly3(5, new Point*[5]);
+	Polygon poly4(5, new Point*[5]);
+	/*Polygon poly1(4, new Point*[4]);
 	Polygon poly2(4, new Point*[4]);
 	Polygon poly3(4, new Point*[4]);
-	Polygon poly4(4, new Point*[4]);
-
+	Polygon poly4(4, new Point*[4]);*/
 
 	thread t0(&GridSimplifierM::simplifyTP, this, map->lines[0], poly1, 0);
 	thread t1(&GridSimplifierM::simplifyTP, this, map->lines[1], poly2, 1);
